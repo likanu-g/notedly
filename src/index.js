e93321869583af0c2840c0b728a67f5a7b29153f
likanu-g <1452930742@qq.com>
2023-03-19 19:35:43 +0800

    更新index.js以及更新相关依赖

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const app = express();
+app.get('/', (req, rep) => rep.send('Hello world'));
+app.listen(4000, () => console.log('Listening on port 4000!'));
