139782da55ca0c28784df5b83d2b25df4fee4122
likanu-g <1452930742@qq.com>
2023-03-20 21:17:56 +0800

    1、新增插入的gql API 2、新增mongoDB的配置

--- src/index.js
+++ src/index.js
@@ -1,19 +1,57 @@
 const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
-const app = express();
+require('dotenv').config();
+const db = require('./db');
+
 const PORT = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
+
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note', author: 'Riley Harrison' }
+];
+
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!, author: String): Note!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello World'
+    hello: () => 'Hello World',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: args.author
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
   }
 };
-
+const app = express();
+db.connect(DB_HOST);
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: '/api' });
 app.get('/', (req, rep) => rep.send('Hello Web Server'));
